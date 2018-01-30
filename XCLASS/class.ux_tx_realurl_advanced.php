b37275dc94bc64bc6e980eb9e6127fd17a6d30ed
Dan Untenzu <untenzu@webit.de>
2018-01-30 09:38:01 +0100

    [FEATURE] CGL
    * Remove dead code
    * Apply Code Style

--- XCLASS/class.ux_tx_realurl_advanced.php
+++ XCLASS/class.ux_tx_realurl_advanced.php
@@ -4,21 +4,17 @@
 
 class ux_tx_realurl_advanced extends tx_realurl_advanced
 {
-
-    function encodeTitle($title)
+    public function encodeTitle($title)
     {
-
         $config_language  = $GLOBALS['TSFE']->tmpl->setup['config.']['language'];
         $domain  = explode('.', $_SERVER['SERVER_NAME']);
 
         # if ($config_language == 'fi')
-        if (in_array('fi',$domain) ) {
-            $trans_suomi_umlaute = array ("ä"=>'a',"å"=>"a","ö"=>"o","Ä"=>"A","Ö"=>"O" );
+        if (in_array('fi', $domain)) {
+            $trans_suomi_umlaute = array("ä"=>'a',"å"=>"a","ö"=>"o","Ä"=>"A","Ö"=>"O" );
             $title =strtr($title, $trans_suomi_umlaute);
         }
 
-       return  parent::encodeTitle($title);
+        return  parent::encodeTitle($title);
     }
-
-
-} 
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,7 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die('Access denied.');
 }
 
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realurl/class.tx_realurl_advanced.php'] =  t3lib_extMgm::extPath('realurl_suomiurl') . "XCLASS/class.ux_tx_realurl_advanced.php";
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_realurl_advanced']['className']='ux_tx_realurl_advanced';
-
-
-?>
-
-
-
-
